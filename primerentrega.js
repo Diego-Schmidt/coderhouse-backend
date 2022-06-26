d2cb62b26836764b3f7af5522b9c90de85ca11e1
Diego Schmidt <email@diegoschmidt.com>
2022-06-26 04:15:47 -0300

    Cambios estéticos en el console.log

--- primerentrega.js
+++ primerentrega.js
@@ -9,27 +9,32 @@ class Usuario {
 }
 // Método para obtener el nombre completo del Usuario
     getFullName(){
-        return `El nombre y apellido de ${this.nombre} es: ${this.nombre} ${this.apellido}`;  
+        console.log('\x1b[33m%s\x1b[0m',`-----> Ejecutando método para obtener el nombre completo del usuario ${this.nombre} <-----`);
+        return `El nombre y apellido del usuario ${this.nombre} es: ${this.nombre} ${this.apellido}`;  
     }
 
 // Método para agregar una mascota a la lista de mascotas del usuario
     addMascota(a){
     this.mascotas.push(a);
-    console.log(`Agregada mascota ${a} a la lista de ${this.nombre}`);    
+    console.log('\x1b[33m%s\x1b[0m',`-----> Ejecutando método para agregar mascota a la lista de mascotas del usuario ${this.nombre} <-----`);
+    console.log(`Agregada mascota ${a} a la lista de ${this.nombre} ${this.apellido}`);    
     }
 // Método para obtener la cantidad de mascotas del usuario
     countMascotas(){
        let mascoPl = (this.mascotas.length > 1) ? 'mascotas' : 'mascota';
-       return `El usuario ${this.nombre} tiene ${this.mascotas.length} ${mascoPl}`;
+       console.log('\x1b[33m%s\x1b[0m',`-----> Ejecutando método para contar la cantidad de mascotas del usuario ${this.nombre} <-----`);
+       return `El usuario ${this.nombre} ${this.apellido} tiene ${this.mascotas.length} ${mascoPl}`;
           
  }
 // Método para agregar un libro a la lista de libros del usuario    
     addBook(titulo, autor) {
+        console.log('\x1b[33m%s\x1b[0m',`-----> Ejecutando método para agregar un libro a la lista de libros del usuario ${this.nombre} <-----`);
         console.log(`Agregado libro ${titulo} a la lista de libros del usuario ${this.nombre}`);
         this.libros.push({nombre: titulo, autor: autor});
     }
 // Método para obtener la lista de libros favoritos del usuario
     getBookNames() {
+        console.log('\x1b[33m%s\x1b[0m',`-----> Ejecutando método para obtener la lista de libros favoritos del usuario ${this.nombre} <-----`);
         return `Esta es la lista de libros favoritos de ${this.nombre}: ${this.libros.map( (libro) => libro.nombre)}`;
     }
 }
@@ -38,7 +43,8 @@ class Usuario {
 const Juan = new Usuario('Juan','Schmidt',[{nombre: "El monstruo subatómico", autor: "Isaac Asimov"} , {nombre: "20 mil leguas de viaje submarino" , autor: "Julio Verne"}],['cata','Paco']);
 
 // Aplicamos todos los métodos 
-
+// Indicamos el inicio de los procesos
+console.log('\x1b[36m%s\x1b[0m', "Inicio de ejecución de los métodos de la clase Usuario para el desafío Clases");
 // Pedimos el nombre completo del usuario Juan usando el método getFullName y lo mostramos en un console.log
 console.log(Juan.getFullName());
 // Pedimos la lista completa de libros favoritos del usuario Juan usando el método getBookNames y lo mostramos en un console.log
@@ -51,3 +57,4 @@ console.log(Juan.countMascotas());
 // Agregamos una mascota a la lista de mascotas de Juan y volvermos a pedir la cantidad de mascotas del usuario Juan usando el método countMascotas y la mostramos en un console.log
 Juan.addMascota("Rintintin");
 console.log(Juan.countMascotas());
+console.log('\x1b[36m%s\x1b[0m',"Fin de ejecución del desafío Clases, saludos a todos :D");
